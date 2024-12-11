d6d2e1bd19d6d96a9f50f4d7baf087f84b156b45
Lim <Joouuuzzzz@yandex.ru>
2024-12-11 21:15:44 +0400

    One

---
+++ ДЗ_1.py
@@ -0,0 +1,32 @@
+HELP = """
+help - напечатать справку по программе.
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
+show - напечатать все добавленные задачи."""
+
+tasks = []
+
+run = True
+
+while run:
+    command = input("Введите команду: ")
+    if command == "help":
+        print(HELP)
+    elif command == "show":
+        print(tasks)
+    elif command == "add":
+        task = input("Введите название задачи: ")
+        tasks.append(task)
+        print("Задача добавлена")
+    elif command == 'exit':
+        print('Спасибо за использование! До свидания!')
+    else: 
+        print("Неизвестная команда")
+        break
+
+print('До свидания')
+
+
+command = input("Введите команду: ")
+x = "exit"
+if command == x:
+    print("Спасибо за использование! До свидания!")
